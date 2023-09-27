812a1882f31b7b347cc2288303a79756ee02b49a
Smartman Apps <20635275+SmartmanApps@users.noreply.github.com>
2023-09-27 15:42:08 +1000

    Add project files.

---
+++ TestImages/TestDxIconPage.cs
@@ -0,0 +1,24 @@
+﻿namespace SmartmanApps.Controls;
+public class TestDxIconPage :ContentPage
+{
+public TestDxIconPage() {
+    Label newLabel=new Label{
+        Text=$"{nameof(TestDxIconPage)}",
+        };
+    Image dotnetBotImage=new Image{
+        Source="dotnet_bot.png",
+//        Source="dropboxicon.png",
+//        HeightRequest=200,
+        HorizontalOptions=LayoutOptions.Center,
+        };
+    Image dropboxImage=new Image{
+        Source="dropbox1.png",
+//        Source="dotnet_bot.png",
+        HeightRequest=200,
+        HorizontalOptions=LayoutOptions.Center,
+        };
+//    Content=newLabel;
+//    Content=dotnetBotImage;
+    Content=dropboxImage;
+    }
+}
